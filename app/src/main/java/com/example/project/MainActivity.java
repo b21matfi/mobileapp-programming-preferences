fe9c4a782a09740345fc57d91362d93918b98986
Mateja <b21matfi@student.his.se>
2022-06-07 21:09:21 +0200

    Added a button and a secondactivity, started on the shared prefrences in mainacitivty

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,18 +1,43 @@
 package com.example.project;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView sharedview;
+    SharedPreferences sharedpref;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        sharedview = findViewById(R.id.sharedview);
+        final Button change = findViewById(R.id.change_button);
+        change.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
+    @Override
+    protected void onResume (){
+        super.onResume();
+
+
+        String login = sharedpref.getString("username", "unknown");
+        sharedview.setText(login);
+
+    }
 }
